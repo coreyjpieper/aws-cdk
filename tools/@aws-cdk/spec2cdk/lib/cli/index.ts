0fd9f1730525560e222188a47a34196289d5157b
Momo Kornher <kornherm@amazon.co.uk>
2023-11-08 15:20:43 +0000

    chore(spec2cdk): fix cli tool for testing

--- tools/@aws-cdk/spec2cdk/lib/cli/index.ts
+++ tools/@aws-cdk/spec2cdk/lib/cli/index.ts
@@ -1,18 +1,94 @@
-import * as path from 'path';
-import { parseArgv } from './args';
+import * as path from 'node:path';
+import { parseArgs } from 'node:util';
+import { PositionalArg, showHelp } from './help';
 import { PatternKeys, generate, generateAll } from '../generate';
 import { log, parsePattern } from '../util';
 
+const command = 'spec2cdk';
+const args: PositionalArg[] = [{
+  name: 'output-path',
+  required: true,
+  description: 'The directory the generated code will be written to',
+}];
+const config = {
+  'help': {
+    short: 'h',
+    type: 'boolean',
+    description: 'Show this help',
+  },
+  'debug': {
+    type: 'boolean',
+    description: 'Show additional debug output',
+  },
+  'pattern': {
+    type: 'string',
+    default: '%moduleName%/%serviceShortName%.generated.ts',
+    description: 'File and path pattern for generated files',
+  },
+  'augmentations': {
+    type: 'string',
+    default: '%moduleName%/%serviceShortName%-augmentations.generated.ts',
+    description: 'File and path pattern for generated augmentations files',
+  },
+  'metrics': {
+    type: 'string',
+    default: '%moduleName%/%serviceShortName%-canned-metrics.generated.ts',
+    description: 'File and path pattern for generated canned metrics files ',
+  },
+  'service': {
+    type: 'string',
+    description: 'Generate files only for a specific service, e.g. aws-lambda',
+  },
+  'clear-output': {
+    type: 'boolean',
+    default: false,
+    description: 'Completely delete the output path before generating new files',
+  },
+  'augmentations-support': {
+    type: 'boolean',
+    default: false,
+    description: 'Generates additional files required for augmentation files to compile. Use for testing only',
+  },
+} as const;
+
+const helpText = `Path patterns can use the following variables:
+
+    %moduleName%          The name of the module, e.g. aws-lambda
+    %serviceName%         The full name of the service, e.g. aws-lambda
+    %serviceShortName%    The short name of the service, e.g. lambda
+
+Note that %moduleName% and %serviceName% can be different if multiple services are generated into a single module.`;
+
+const help = () => showHelp(command, args, config, helpText);
+
 async function main(argv: string[]) {
-  const { args, options } = parseArgv(argv, ['output']);
+  const {
+    positionals,
+    values: options,
+  } = parseArgs({
+    args: argv,
+    allowPositionals: true,
+    options: config,
+  });
+
+  if (options.help) {
+    help();
+    return;
+  }
+
   if (options.debug) {
     process.env.DEBUG = '1';
   }
-  log.debug('CLI args', args, options);
+  log.debug('CLI args', positionals, options);
+
+  const outputDir = positionals[0];
+  if (!outputDir) {
+    throw new Error('Please specify the output-path');
+  }
 
   const pss: Record<PatternKeys, true> = { moduleName: true, serviceName: true, serviceShortName: true };
 
-  const outputPath = args.output ?? path.join(__dirname, '..', 'services');
+  const outputPath = outputDir ?? path.join(__dirname, '..', 'services');
   const resourceFilePattern = parsePattern(
     stringOr(options.pattern, path.join('%moduleName%', '%serviceShortName%.generated.ts')),
     pss,
@@ -35,8 +111,8 @@ async function main(argv: string[]) {
       augmentations: augmentationsFilePattern,
       cannedMetrics: cannedMetricsFilePattern,
     },
-    clearOutput: !!options['clear-output'],
-    augmentationsSupport: !!options['augmentations-support'],
+    clearOutput: options['clear-output'],
+    augmentationsSupport: options['augmentations-support'],
     debug: options.debug as boolean,
   };
 
@@ -50,7 +126,8 @@ async function main(argv: string[]) {
 }
 
 main(process.argv.splice(2)).catch((e) => {
-  log.error(e);
+  log.error(`Error: ${e.message}\n`);
+  showHelp(command, args);
   process.exitCode = 1;
 });
 
